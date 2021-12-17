190f5c6fc81a75add2be54d442e90a268dee7162
Laurent Laborde <kerdezixe@gmail.com>
2021-12-17 14:07:49 +0100

    it works

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 # %%
-import tensorflow as tf
 import pathlib
+
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from skimage.color import rgb2lab, lab2rgb, rgb2ycbcr, ycbcr2rgb
-from tensorflow.keras.utils import plot_model
+import tensorflow as tf
+import time
 
-EPOCHS = 8
-#LR = 0.0002
-LR =  0.0003
-BATCH_SIZE = 2
-DATASET_SIZE = 0 #Set to 0 for all data
+start = time.time()
 
-#tf.keras.mixed_precision.set_global_policy('mixed_float16')
+EPOCHS = 64
+# LR = 0.0002
+LR = 0.0002
+BATCH_SIZE = 4
+DATASET_SIZE = 128  # Set to 0 for all data
+
+# tf.keras.mixed_precision.set_global_policy('mixed_float16')
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-from tensorflow.python.framework.ops import disable_eager_execution
-#disable_eager_execution()
-#print(tf.executing_eagerly())
+# disable_eager_execution()
+# print(tf.executing_eagerly())
 
 datadir = pathlib.Path("/Volumes/T7/coco/recolorize/color")
-ds_list = tf.data.Dataset.list_files(str(datadir/'0*.jpg'), shuffle=True)
+ds_list = tf.data.Dataset.list_files(str(datadir / '0*.jpg'), shuffle=True)
 
 # limit the size of the dataset
 if DATASET_SIZE != 0:
     ds_list = ds_list.take(DATASET_SIZE)
 
-def process_image(file_path):
-    file = tf.io.read_file(file_path)
+
+def process_image(_file_path):
+    file = tf.io.read_file(_file_path)
     color = tf.image.decode_jpeg(file, channels=3)
     color = tf.image.random_flip_left_right(color)
     color = tf.image.random_flip_up_down(color)
@@ -42,46 +43,46 @@ def process_image(file_path):
     color = tf.image.rgb_to_yiq(color)
     return bw, color
 
+
 image_ds = ds_list.map(process_image)
-#image_ds = image_ds0.cache() # cache in memory (!!!)
+# image_ds = image_ds0.cache() # cache in memory (!!!)
 
 image_tensors = image_ds.batch(BATCH_SIZE, num_parallel_calls=tf.data.AUTOTUNE)
-ds = image_tensors.prefetch(buffer_size = tf.data.AUTOTUNE)
-
+ds = image_tensors.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 model = tf.keras.Sequential()
-#model.add(tf.keras.layers.DepthwiseConv2D((3, 3), activation='tanh', padding='same', strides=1))
+# model.add(tf.keras.layers.DepthwiseConv2D((3, 3), activation='tanh', padding='same', strides=1))
 model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='tanh', padding='same', strides=1, data_format="channels_last",
                                  input_shape=(128, 128, 3)))
 
 model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same', strides=1))
-model.add(tf.keras.layers.Dropout(0.5)) # randomly drop some neurons
+model.add(tf.keras.layers.Dropout(0.5))  # randomly drop some neurons
 model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', strides=1))
-model.add(tf.keras.layers.MaxPool2D(2,2)) # divide the image by 2
+model.add(tf.keras.layers.MaxPool2D(2, 2))  # divide the image by 2
 model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', strides=1))
-#model.add(tf.keras.layers.MaxPool2D(2,2)) # divide the image by 2
-#model.add(tf.keras.layers.Conv2D(256, (6, 6), activation='swish', padding='same', strides=1))
+# model.add(tf.keras.layers.MaxPool2D(2,2)) # divide the image by 2
+# model.add(tf.keras.layers.Conv2D(256, (6, 6), activation='swish', padding='same', strides=1))
 
-#model.add(tf.keras.layers.Conv2DTranspose(256, kernel_size=6, strides=1, activation='swish', padding='same'))
+# model.add(tf.keras.layers.Conv2DTranspose(256, kernel_size=6, strides=1, activation='swish', padding='same'))
 model.add(tf.keras.layers.Conv2DTranspose(64, kernel_size=3, strides=1, activation='relu', padding='same'))
 model.add(tf.keras.layers.Conv2DTranspose(64, kernel_size=3, strides=1, activation='relu', padding='same'))
-model.add(tf.keras.layers.UpSampling2D((2,2))) # rescale x2
+model.add(tf.keras.layers.UpSampling2D((2, 2)))  # rescale x2
 model.add(tf.keras.layers.Conv2DTranspose(32, kernel_size=3, strides=1, activation='relu', padding='same'))
 model.add(tf.keras.layers.Conv2DTranspose(16, kernel_size=3, strides=1, activation='relu', padding='same'))
 model.add(tf.keras.layers.Conv2DTranspose(8, kernel_size=3, strides=1, activation='relu', padding='same'))
 
 model.add(tf.keras.layers.Conv2D(3, kernel_size=(3, 3), activation="tanh", padding='same', data_format="channels_last"))
 
-#layer = model.layers
-#filters, biases = model.layers[1].get_weights()
-#print(layer[1].name, filters.shape)
+# layer = model.layers
+# filters, biases = model.layers[1].get_weights()
+# print(layer[1].name, filters.shape)
 
 model.compile(loss=tf.keras.losses.MeanSquaredError(),
               optimizer=tf.keras.optimizers.Adam(learning_rate=LR),
               metrics=["accuracy", "mae", "mse"]
               )
 
-#history = model.fit(ds, batch_size=BATCH_SIZE, shuffle=True, epochs=EPOCHS)
+# history = model.fit(ds, batch_size=BATCH_SIZE, shuffle=True, epochs=EPOCHS)
 history = model.fit(ds, shuffle=False, epochs=EPOCHS)
 
 hist = pd.DataFrame(history.history)
@@ -89,10 +90,10 @@ hist['epoch'] = history.epoch
 hist.tail()
 
 
-def plot_loss(history):
+def plot_loss(_history):
     plt.figure(1)
-#    plt.plot(history.history['val_loss'], label='validation loss')
-    plt.plot(history.history['loss'], label='training loss')
+    #    plt.plot(history.history['val_loss'], label='validation loss')
+    plt.plot(_history.history['loss'], label='training loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.ylim((0, 0.01))
@@ -104,8 +105,8 @@ def plot_loss(history):
 plot_loss(history)
 
 # show model
-#model.summary()
-#plot_model(model, "model.png", show_shapes=True, show_dtype=True)
+# model.summary()
+# plot_model(model, "model.png", show_shapes=True, show_dtype=True)
 
 # get input image
 file_path = next(iter(ds_list))
@@ -115,7 +116,7 @@ img = image_ds.batch(1)
 
 # display original
 plt.figure(3)
-#out *= 255
+# out *= 255
 plt.imshow(out)
 plt.show()
 
@@ -124,7 +125,7 @@ plt.figure(2)
 plt.axis("off")
 predictions = model.predict(img)
 predictions = tf.image.yiq_to_rgb(predictions)
-#predictions /= 255.0
+# predictions /= 255.0
 plt.imshow(tf.squeeze(predictions))
 plt.show()
 
@@ -135,9 +136,11 @@ plt.show()
 # plt.imshow(tf.squeeze(original))
 # plt.show()
 
-#plt.figure(4)
-#original = tf.cast(original,tf.double)
-#predictions = tf.cast(predictions,tf.double)
-#ssim = tf.image.ssim(original, predictions, 255)
-#plt.imshow(ssim)
-#plt.show()
+# plt.figure(4)
+# original = tf.cast(original,tf.double)
+# predictions = tf.cast(predictions,tf.double)
+# ssim = tf.image.ssim(original, predictions, 255)
+# plt.imshow(ssim)
+# plt.show()
+
+print("elapsed : ", time.time() - start)
